25171cd7f29304697f02341d0f8989c880c45310
DocDuck <alex.mitin.psmu@gmail.com>
2024-01-04 16:17:55 +0500

    кластеризация k-means

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
-from numpy import NaN
-import torch
+from numpy import NaN, ndarray
 # Диапазон тензоров и тензоры по подобию имеющихся
 # master_range = torch.range(start=1, step=3, end=333)
 
@@ -93,10 +93,89 @@ import torch
 # BR = Q75 + 1.5 *(Q75 - Q25)
 # print(np.arange(BL, BR))
 # Проверяем является ли значение выбросом методом среднего значения, отклонения и медианы
-av = 10 # среднее значение
-S = 1.1 # отклонение
-m = 9 # медиана
-# так как медиана и среднее близки то выборка симметричная - коэффициент равен 3 (для нессиметричных было бы 5)
-BL =  av - 3 * S # левый край интервала
-BR = av + 3 * S # правый край интервала
-print(np.arange(BL, BR))
+# av = 10 # среднее значение
+# S = 1.1 # отклонение
+# m = 9 # медиана
+# # так как медиана и среднее близки то выборка симметричная - коэффициент равен 3 (для нессиметричных было бы 5)
+# BL =  av - 3 * S # левый край интервала
+# BR = av + 3 * S # правый край интервала
+# print(np.arange(BL, BR))
+# Функция находит расстояния между объектами выборки методом Манхеттен 
+# def manhet(list1, list2, list3, list4):
+#     s = 0
+#     for a, b, c, d in zip(list1, list2, list3, list4):
+#         s += abs(a - b) + abs(a - c) + abs(a - d)
+#     return s
+
+# a = [1, 1, 0]
+# b = [0, 2, -1]
+# c = [2, 3, 1]
+# d = [1, 0, 4]
+
+# print('a:', manhet(a, b, c, d))
+# print('b:', manhet(b, c, d, a))
+# print('c:', manhet(c, b, a, d))
+# print('d:', manhet(d, b, c, a))
+
+# Реализация алгоритма к-средних (k-means) для кластеризации объектов по заранее известному количеству кластеров
+# 1 Создадим пару массивов np.array с признаками
+X = np.array([[4,2],[3,2],[1,-1],[-1,1],[0,4]])
+# Создадим 2 центра масс для 2х кластеров
+C = np.array([[2,3], [1,1]])
+# Расстояние Манхэттена. Принимает 2 кортежа с признаками [Pа1, Pб1] объекта А
+manhattan = lambda X, Y: np.abs(X - Y).sum(axis=1)
+# Реализуем класс KMeans
+class KMeans:
+    # Инициализация.
+    # n_clusters - желаемое число кластеров. 
+    # centers - начальные значения центров кластеризации.
+    # max_iter - максимальное число итераций алгоритма.
+    # metric - метрика для расчета расстояний между центрами кластеризации и точек данных. (функция принимающая 2 числа и возвращающая число)
+    def __init__(self, n_clusters, centers, max_iter, metric):
+        self.n_clusters = n_clusters
+        self.centers = centers
+        self.max_iter = max_iter
+        self.metric = metric
+    @staticmethod
+    def __euclid(X1, X2):
+        # находим сумму квадратов расстояний между точками
+        sum_sq = np.sum(np.square(X1 - X2))
+        return sum_sq**0.5
+    # Найти центр масс точек.
+    @staticmethod
+    def __mass_center(X: ndarray[int]):
+        return X.mean(axis=0)
+    # Кластеризуем
+    def fit(self, X):
+        distances = np.zeros((self.n_clusters, len(X))) # Расстояния от центров кластеризации до точек.
+        self.n_iters = 0                                # Число итераций.
+        for _ in range(self.max_iter):
+            # Найти расстояния между центрами кластеризации и точками.
+            for i in range(self.n_clusters):
+                distances[i] = self.metric(self.centers[i], X)
+            
+            # Найти метки точек.
+            self.labels = distances.argmin(axis=0)
+
+            # Найти новые значения центров кластеризации.
+            prev_centers = self.centers.copy()
+            for i in np.unique(self.labels):
+                self.centers[i] = self.__mass_center(X[self.labels == i])
+            
+            self.n_iters += 1 # Подсчитать число итераций алгоритма.
+            # Если центры кластеризации практически не меняются, завершить алгоритм. 
+            if self.__euclid(self.centers, prev_centers) < 10**-4:
+                break
+        return self
+
+centers = np.random.randn(3, 2)
+# Кластеризовать данные.
+kmean = KMeans(n_clusters=2, centers=C, max_iter=100, metric=manhattan).fit(X)
+print(f"Iterations: {kmean.n_iters}") # Вывести число итераций алгоритма.
+
+# Отобразить кластеризованные данные и их центры масс. 
+plt.scatter(X[:, 0], X[:, 1], c=kmean.labels)
+plt.scatter(kmean.centers[:, 0], kmean.centers[:, 1], c='red')
+plt.show()
+
+
